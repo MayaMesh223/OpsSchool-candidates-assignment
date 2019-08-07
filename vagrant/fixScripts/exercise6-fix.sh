6040969070c1296d3f3126ddd98794850ac44f0d
MayaMesh223 <maya.meshoulam@gmail.com>
2019-08-07 19:43:22 +0300

    adding OpsSchool exercise fixes

--- vagrant/fixScripts/exercise6-fix.sh
+++ vagrant/fixScripts/exercise6-fix.sh
@@ -1,2 +1,25 @@
 #!/bin/bash
-#add fix to exercise6-fix here
+
+#defining which server I am on and what the destination is
+if [[ $HOSTNAME == "server1" ]];
+then
+ destServer=server2
+else
+ destServer=server1
+fi
+
+target=${!#}
+totalSize=0
+
+# copying files and finding file size in bytes
+for argval in "$@"
+do
+  if [[ $argval != $target ]];
+  then
+   scp -r $argval vagrant@$destServer:$target
+   filesize=$(stat -c%s $argval)
+   ((totalSize+=$filesize))
+  fi
+done
+
+echo "$totalSize"
